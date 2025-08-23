5978251f9ed81b472b897916b15442aac4ffeb1e
Danilo Ferreira <danilo231105@gmail.com>
2025-08-22 21:55:13 -0300

    exercicio 5

---
+++ lista_exercicios-1/exercicio5.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>MÉDIA</h1>
+<form method="post">
+            <div class="mb-3">
+              <label for="numero" class="form-label">n1</label>
+              <input type="number" id="n1" name="n1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="n2" class="form-label">n2</label>
+              <input type="number" id="n2" name="n2" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="n3" class="form-label">n3</label>
+              <input type="number" id="n3" name="n3" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $n1 = $_POST["n1"];
+        $n2 = $_POST["n2"];
+        $n3 = $_POST["n3"];
+        $media = ($n1 + $n2 + $n3) / 3;
+        echo "<p>Média: $media </p>";
+
+    }
+
+
+
+?>
+
+
+
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
