902435c0c3b9f62caf0088ac4bb064d9259b7e5d
Danilo Ferreira <danilo231105@gmail.com>
2025-09-04 21:05:39 -0300

    ex14 lista 1

---
+++ lista_exercicios-1/exercicio14.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post">
+<div class="mb-3">
+              <label for="metros" class="form-label">Insira o valor em km</label>
+              <input type="number" id="km" name="km" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $km = $_POST["km"];
+        $milha = $km * 0.621371;
+        echo "$km km são $milha milhas";
+    
+    
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
