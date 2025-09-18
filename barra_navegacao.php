a84bcdaea22d49d84d55d87e47fb573d5dc57d9a
Danilo Ferreira <danilo231105@gmail.com>
2025-09-18 20:19:00 -0300

    add cabeçalho do amigo (pois faltei na aula)

--- barra_navegacao.php
+++ barra_navegacao.php
@@ -25,11 +25,11 @@
     Exercícios Bootstrap
   </a>
   <ul class="dropdown-menu" aria-labelledby="dropdown2">
-    <li><a class="dropdown-item" href="/Exercicios_Bootstrap/form1.php">Formulário 1</a></li>
-<li><a class="dropdown-item" href="/Exercicios_Bootstrap/form2.php">Formulário 2</a></li>
-<li><a class="dropdown-item" href="/Exercicios_Bootstrap/form3.php">Formulário 3</a></li>
-<li><a class="dropdown-item" href="/Exercicios_Bootstrap/form4.php">Formulário 4</a></li>
-<li><a class="dropdown-item" href="/Exercicios_Bootstrap/form5.php">Formulário 5</a></li>
+    <li><a class="dropdown-item" href="/formularios/form1.php">Formulário 1</a></li>
+<li><a class="dropdown-item" href="/formularios/form2.php">Formulário 2</a></li>
+<li><a class="dropdown-item" href="/formularios/form3.php">Formulário 3</a></li>
+<li><a class="dropdown-item" href="/formularios/form4.php">Formulário 4</a></li>
+<li><a class="dropdown-item" href="/formularios/form5.php">Formulário 5</a></li>
   </ul>
 </li>
 
@@ -38,26 +38,26 @@
     Lista de Exercícios 1
   </a>
   <ul class="dropdown-menu" aria-labelledby="dropdown3">
-    <li><a class="dropdown-item" href="/Exercicios/ex1.php">Exercício 1 - SOMATÓRIO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex2.php">Exercício 2 - SUBTRAÇÃO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex3.php">Exercício 3 - MULTIPLICAÇÃO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex4.php">Exercício 4 - DIVISÃO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex5.php">Exercício 5 - MÉDIA DE NOTAS (P1,P2,P3)</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex6.php">Exercício 6 - CONVERSÃO PARA FAHRENHEIT</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex7.php">Exercício 7 - CONVERSÃO PARA CELSIUS</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex8.php">Exercício 8 - ÁREA DO RETÂNGULO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex9.php">Exercício 9 - ÁREA DO CÍRCULO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex10.php">Exercício 10 - PERÍMETRO DO RETÂNGULO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex11.php">Exercício 11 - PERÍMETRO DO CÍRCULO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex12.php">Exercício 12 - BASE E EXPOENTE</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex13.php">Exercício 13 - CONVERSÃO METROS EM CENTÍMETROS</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex14.php">Exercício 14 - CONVERSÃO KM EM MILHAS</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex15.php">Exercício 15 - IMC</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex16.php">Exercício 16 - PREÇO E DESCONTO</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex17.php">Exercício 17 - RENDIMENTO (JUROS SIMPLES)</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex18.php">Exercício 18 - RENDIMENTO (JUROS COMPOSTO)</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex19php">Exercício 19 - CONVERSÃO DIAS, HORAS, MINUTOS E SEGUNDOS</a></li>
-<li><a class="dropdown-item" href="/Exercicios/ex20.php">Exercício 20 - MÉDIA DE VELOCIDADE</a></li>
+    <li><a class="dropdown-item" href="/lista_exercicios-1/exercicio1.php">Exercício 1 - SOMATÓRIO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio2.php">Exercício 2 - SUBTRAÇÃO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio3.php">Exercício 3 - MULTIPLICAÇÃO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio4.php">Exercício 4 - DIVISÃO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio5.php">Exercício 5 - MÉDIA DE NOTAS (P1,P2,P3)</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio6.php">Exercício 6 - CONVERSÃO PARA FAHRENHEIT</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio7.php">Exercício 7 - CONVERSÃO PARA CELSIUS</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio8.php">Exercício 8 - ÁREA DO RETÂNGULO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio9.php">Exercício 9 - ÁREA DO CÍRCULO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio10.php">Exercício 10 - PERÍMETRO DO RETÂNGULO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio11.php">Exercício 11 - PERÍMETRO DO CÍRCULO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio12.php">Exercício 12 - BASE E EXPOENTE</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio13.php">Exercício 13 - CONVERSÃO METROS EM CENTÍMETROS</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio14.php">Exercício 14 - CONVERSÃO KM EM MILHAS</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio15.php">Exercício 15 - IMC</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio16.php">Exercício 16 - PREÇO E DESCONTO</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio17.php">Exercício 17 - RENDIMENTO (JUROS SIMPLES)</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio18.php">Exercício 18 - RENDIMENTO (JUROS COMPOSTO)</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio19php">Exercício 19 - CONVERSÃO DIAS, HORAS, MINUTOS E SEGUNDOS</a></li>
+<li><a class="dropdown-item" href="/lista_exercicios-1/exercicio20.php">Exercício 20 - MÉDIA DE VELOCIDADE</a></li>
   </ul>
 </li>
       </ul>
