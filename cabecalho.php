9374386e8171545818832b6dfd97fc4e9627c175
Danilo Ferreira <danilo231105@gmail.com>
2025-09-18 20:20:02 -0300

    add rodapé do amigo (pois faltei na aula)

---
+++ cabecalho.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>FATEC</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">FATEC</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+<li class="nav-item">
+  <a class="nav-link" aria-current="page" href="#">Início</a>
+</li>
+
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Exercícios Bootstrap
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdown2">
+    <li><a class="dropdown-item" href="/formularios/form1.php">Formulário 1</a></li>
+<li><a class="dropdown-item" href="/formularios/form2.php">Formulário 2</a></li>
+<li><a class="dropdown-item" href="/formularios/form3.php">Formulário 3</a></li>
+<li><a class="dropdown-item" href="/formularios/form4.php">Formulário 4</a></li>
+<li><a class="dropdown-item" href="/formularios/form5.php">Formulário 5</a></li>
+  </ul>
+</li>
+
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Lista de Exercícios 1
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdown3">
+    <li><a class="dropdown-item" href="/lista-exercicio-2/exercicio1.php">Exercício 1 - SOMATÓRIO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio2.php">Exercício 2 - SUBTRAÇÃO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio3.php">Exercício 3 - MULTIPLICAÇÃO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio4.php">Exercício 4 - DIVISÃO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio5.php">Exercício 5 - MÉDIA DE NOTAS (P1,P2,P3)</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio6.php">Exercício 6 - CONVERSÃO PARA FAHRENHEIT</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio7.php">Exercício 7 - CONVERSÃO PARA CELSIUS</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio8.php">Exercício 8 - ÁREA DO RETÂNGULO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio9.php">Exercício 9 - ÁREA DO CÍRCULO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio10.php">Exercício 10 - PERÍMETRO DO RETÂNGULO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio11.php">Exercício 11 - PERÍMETRO DO CÍRCULO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio12.php">Exercício 12 - BASE E EXPOENTE</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio13.php">Exercício 13 - CONVERSÃO METROS EM CENTÍMETROS</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio14.php">Exercício 14 - CONVERSÃO KM EM MILHAS</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio15.php">Exercício 15 - IMC</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio16.php">Exercício 16 - PREÇO E DESCONTO</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio17.php">Exercício 17 - RENDIMENTO (JUROS SIMPLES)</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio18.php">Exercício 18 - RENDIMENTO (JUROS COMPOSTO)</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio19.php">Exercício 19 - CONVERSÃO DIAS, HORAS, MINUTOS E SEGUNDOS</a></li>
+<li><a class="dropdown-item" href="/lista-exercicio-2/exercicio20.php">Exercício 20 - MÉDIA DE VELOCIDADE</a></li>
+  </ul>
+</li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container py-3">
